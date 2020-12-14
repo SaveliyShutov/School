3dae0d54f8765ec217978f0f1464892331eab710
tooortttt <savelijsutov@gmail.com>
2020-12-14 19:39:07 +0400

    hello

---
+++ firebaseinit.js
@@ -0,0 +1,13 @@
+import firebase from 'firebase/app'
+var firebaseConfig = {
+    apiKey: "AIzaSyB5UItKwsLCS3LrbWeZe5yujW5GgD1HvbU",
+    authDomain: "answer-3ba91.firebaseapp.com",
+    projectId: "answer-3ba91",
+    storageBucket: "answer-3ba91.appspot.com",
+    messagingSenderId: "38605061892",
+    appId: "1:38605061892:web:16539652f74fda2e0a5a51",
+    measurementId: "G-LW9B9MTX50"
+
+  };
+
+export default firebase.initializeApp(firebaseConfig)
--- src/components/Mainpage.vue
+++ src/components/Mainpage.vue
@@ -1,27 +1,92 @@
 <template>
   <div class="main">
-    <div class="md-layout">
-      <div class="md-layout-item image">
-        <p>Hello</p>
-      </div>
+    <div>
+      <form
+        novalidate
+        class="md-layout md-alignment-center-center"
+        @submit.prevent="sendQuestion"
+      >
+        <md-card class="md-layout-item md-layout md-size-50 md-small-size-100">
+          <md-card-header>
+            <div class="md-title">Задай вопрос</div>
+          </md-card-header>
+          <md-field :class="messageClass">
+            <label>Вопрос</label>
+            <md-textarea v-model="textarea" required></md-textarea>
+            <span class="md-error">There is an error</span>
+          </md-field>
+
+          <md-field class="md-layout-item md-size-45 md-small-size-100">
+            <label>Количество баллов</label>
+            <md-input v-model="number" type="number"></md-input>
+          </md-field>
+
+          <md-field class="md-layout-item md-size-45 md-small-size-100">
+            <label>Type here!</label>
+            <md-input v-model="userName"></md-input>
+          </md-field>
+
+          <md-card-actions>
+            <md-button type="submit" class="md-primary" :disabled="sending"
+              >Отправить</md-button
+            >
+          </md-card-actions>
+        </md-card>
+
+        <md-snackbar :md-active.sync="userSaved"
+          >The user {{ lastUser }} was saved with success!</md-snackbar
+        >
+      </form>
     </div>
   </div>
 </template>
 
 <script>
+import firebase from "../../firebaseinit";
+import "firebase/firestore";
+const db = firebase.firestore();
 export default {
   name: "Mainpage",
+  data: function () {
+    return {
+      textarea: "",
+      number: 0,
+      userName: "",
+    };
+  },
+  methods: {
+    sendQuestion: function () {
+      db.collection("Answers")
+        .add(this.createObject())
+        .then(function (docRef) {
+          console.log("Document written with ID: ", docRef.id);
+        })
+        .catch(function (error) {
+          console.error("Error adding document: ", error);
+        });
+    this.textarea="",
+    this.number=0,
+    this.userName=""
+    },
+    createObject: function () {
+      return {
+        textarea: this.textarea,
+        number: this.number,
+        userName: this.userName,
+      };
+    },
+  },
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-.md-layout {
-  height: 100vh;
-  width: 100%;
-  
+.md-field {
+  margin: 20px;
 }
-.md-layout-item {
-  background-image: url(https://goo.su/3evS);
+.md-card {
+  padding: 20px;
+  width: 400px;
 }
 </style>
+
